7f602cae94df35de5b940db3e3dce636b5932efb
unknown <ecblickenderfer@gmail.com>
2023-05-22 17:41:27 -0400

    Added function to creat svg file

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ const inquirer = require("inquirer");
 const fs = require("fs"); 
 const {Triangle, Circle, Square} = require("./lib/shapes");
 
+function createSvgFile(answer){
+    let mySvg = "<svg version = '1.1' width = '300' height = '200' xmlns = 'http://www.w3.org/2000/svg'>"
+    let shapeChoice = new (eval(answer.shape))()
+    shapeChoice.setColor(answer.shapeColor)
+    mySvg += shapeChoice.render()
+    mySvg += `<text x= "150" y = "130" text-anchor = "middle" font-size: "40" fill = "${answer.textColor}"> ${answer.text} </text>`
+    mySvg += "</svg>"
+    fs.writeFile("logo.svg", mySvg, err=>{
+        err?console.log(err): console.log("Generated logo.svg")
+    })
+}
+
 function promptUser(){
     inquirer
         .prompt([
@@ -13,7 +25,7 @@ function promptUser(){
             {
                 type: "input",
                 message: "What color would you like the text to be?",
-                name: "text-color", 
+                name: "textColor", 
             },
             {
                 type: "list",
@@ -24,9 +36,11 @@ function promptUser(){
             {
                 type: "input",
                 message: "What color would you like the shape to be?",
-                name: "shape-color",
+                name: "shapeColor",
             }
 
         ])
-        
+        .then(answer =>{
+            createSvgFile(answer)
+        }) 
 }
